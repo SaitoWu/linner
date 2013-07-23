8af886da7985c056286514d3a216f216a19869d7
Saito <saitowu@gmail.com>
2013-07-23 15:59:57 +0800

    add asset spec

--- spec/linner/asset_spec.rb
+++ spec/linner/asset_spec.rb
@@ -3,6 +3,19 @@ require "spec_helper"
 describe Asset do
 
   before(:each) do
-    @asset = Asset.new("app.js")
+    @asset = Asset.new("app/scripts/app.js")
+  end
+
+  describe :logical_path do
+    it "should be return right logical_path" do
+      @asset.logical_path.should == "app.js"
+    end
+  end
+
+  describe :wrappable do
+    it "should wrapperable" do
+      @asset.wrappable?.should be_true
+      Asset.new("app/styles/app.css").wrappable?.should be_false
+    end
   end
 end
