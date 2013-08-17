b9efa92d0b53e38ea62621e1c5747fd961b060c7
Saito <saitowu@gmail.com>
2013-08-18 04:22:31 +0800

    reduce complexity of order

--- lib/linner/helper.rb
+++ lib/linner/helper.rb
@@ -13,25 +13,17 @@ module Linner
       order_ary = ary.inject([[]]) do |a, x|
         x != "..." ? a.last << x : a<< []; a
       end
-      order_by_before(self, order_ary.first)
-      order_by_after(self, order_ary.last)
+      order_by_direction(order_ary.first, :before)
+      order_by_direction(order_ary.last, :after)
       self
     end
 
   private
-    def order_by_before(list, before)
-      before.reverse.each do |f|
-        if i = list.index {|x| x =~ /#{f}/i}
-          list.unshift list.delete_at i
-        end
-      end
-    end
-
-    def order_by_after(list, after)
-      after.reverse.each do |f|
-        if i = list.index {|x| x =~ /#{f}/i}
-          list.push list.delete_at i
-        end
+    def order_by_direction(ary, direction)
+      ary.reverse.each do |f|
+        next unless i = ary.index {|x| x =~ /#{f}/i}
+        item = self.delete_at i
+        direction == :before ? self.unshift(item) : self.push(item)
       end
     end
   end
