fd99d80d327409f57697c89c310dbbd611b8cf4b
Saito <saitowu@gmail.com>
2014-09-02 11:33:27 +0800

    move the encoding declaration to the entry of program

--- lib/linner.rb
+++ lib/linner.rb
@@ -12,9 +12,6 @@ require "linner/notifier"
 require "linner/compressor"
 require "linner/environment"
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
-
 module Linner
   extend self
 
