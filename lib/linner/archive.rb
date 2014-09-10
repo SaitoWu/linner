eb939feeeb388ef97a919d15f1b808f8bd98b046
Saito <saitowu@gmail.com>
2014-09-10 22:34:48 +0800

    add a archiver for tar.gz file

---
+++ lib/linner/archive.rb
@@ -0,0 +1,50 @@
+require "rubygems/package"
+
+module Linner
+  class Archive
+
+    def tar(path, dest)
+      archived = StringIO.new
+      Gem::Package::TarWriter.new(archived) do |tar|
+        Dir[File.join(path, "**/*")].each do |file|
+          mode = File.stat(file).mode
+          relative_file = file.sub /^#{Regexp.escape(path)}\/?/, ""
+          if File.directory?(file)
+            tar.mkdir relative_file, mode
+          else
+            tar.add_file relative_file, mode do |tf|
+              File.open(file, "rb") { |f| tf.write f.read }
+            end
+          end
+        end
+      end
+
+      archived.rewind
+
+      Zlib::GzipWriter.open(dest) do |gz|
+        gz.write archived.string
+      end
+    end
+
+    def untar(path, dest)
+      extracted = Gem::Package::TarReader.new Zlib::GzipReader.open(path)
+
+      extracted.rewind
+
+      extracted.each do |entry|
+        file = File.join dest, entry.full_name
+        if entry.directory?
+          FileUtils.mkdir_p file
+        else
+          directory = File.dirname(file)
+          FileUtils.mkdir_p directory unless File.directory?(directory)
+          File.open file, "wb" do |f|
+            f.print entry.read
+          end
+        end
+      end
+
+      extracted.close
+    end
+  end
+end
