1ed7642558d28f92222660c747f04abe01b3e291
Saito <saitowu@gmail.com>
2015-03-23 16:29:11 +0800

    add yml yaml and txt to templates list

--- lib/linner/template.rb
+++ lib/linner/template.rb
@@ -5,16 +5,20 @@ require "handlebars.rb"
 require "coffee_script"
 
 module Tilt
+  class YAMLTemplate < PlainTemplate
+    self.default_mime_type = "text/x-yaml"
+  end
+
   class JavascriptTemplate < PlainTemplate
-    self.default_mime_type = 'application/javascript'
+    self.default_mime_type = "application/javascript"
   end
 
   class CSSTemplate < PlainTemplate
-    self.default_mime_type = 'text/css'
+    self.default_mime_type = "text/css"
   end
 
   class CompassSassTemplate < SassTemplate
-    self.default_mime_type = 'text/css'
+    self.default_mime_type = "text/css"
 
   private
     def sass_options
@@ -27,7 +31,7 @@ module Tilt
   end
 
   class CompassScssTemplate < CompassSassTemplate
-    self.default_mime_type = 'text/css'
+    self.default_mime_type = "text/css"
 
   private
     def sass_options
@@ -36,7 +40,7 @@ module Tilt
   end
 
   class HandlebarsTemplate < Template
-    self.default_mime_type = 'text/template'
+    self.default_mime_type = "text/template"
 
     def prepare; end
 
@@ -45,8 +49,10 @@ module Tilt
     end
   end
 
+  register PlainTemplate, "txt"
   register CSSTemplate, "css"
   register JavascriptTemplate, "js"
+  register YAMLTemplate, "yml", "yaml"
   register HandlebarsTemplate, "hbs", "handlebars"
 
   register CompassSassTemplate, "sass"
